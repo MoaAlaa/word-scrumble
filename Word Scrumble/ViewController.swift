22bdc100bd727b0a7668dc3b129c14f57ff53aca
MoaAlaa <moaalaa16@gmail.com>
2018-11-25 09:05:20 +0200

    Initial Commit

---
+++ Word Scrumble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Scrumble
+//
+//  Created by Mohamed Alaa El-Din on 11/25/18.
+//  Copyright © 2018 Mohamed Alaa El-Din. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
