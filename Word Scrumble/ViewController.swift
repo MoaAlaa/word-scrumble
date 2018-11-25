3a901600bdd5853655e8546f4f5bbfdf97d57806
MoaAlaa <moaalaa16@gmail.com>
2018-11-25 10:06:02 +0200

    Implementing The Table View

--- Word Scrumble/ViewController.swift
+++ Word Scrumble/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var allWords = [String]()
+    var usedWords = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = ["MoaAlaa"]
+        }
+        
+        startGame()
+        
+    }
+    
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
     }
 
 
